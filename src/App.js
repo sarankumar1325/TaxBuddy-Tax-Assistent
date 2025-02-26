e67d711fe7f9a24e6ea78563e8d798af3262fe32
Murugan Balachandran <10murugan01@gmail.com>
2025-02-26 13:36:13 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link>
-        </nav>
-        <h1>hello</h1>
-        <h1>hello</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
+const App = () => {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to our React application!</p>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
-function About() {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page of our application.</p>
-    </div>
-  );
-}
-
-export default App;
+export default App
